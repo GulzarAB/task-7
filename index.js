8446710cfd3a73abb03d6629a0313fe5db06e6d4
Gulzar Abdullabaeva <gulzar.abdullabaeva.kk@gmail.com>
2022-02-20 16:10:56 +0600

    fix:fix bug

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const btn = document.querySelector(".btn");
 const div = document.querySelector("div");
 const totoList = document.querySelector(".todo-list") 
 
-btn.addEventListener("click",function () {
+btn.addEventListener("click",(e)=> {
+    e.preventDefault()
      let p = document.createElement("p");
      p.classList.add("todo-item");
      p.innerText = input.value;
      div.appendChild(p);
      input.value = "";
 
-     p.addEventListener("click",function(){
+     p.addEventListener("click",(e)=>{
+         e.preventDefault()
          div.removeChild(p);
      })
 
